23b6acb38b8dba348aba27f7c85315c05c7484e1
Gallery <59381987+Gallerys@users.noreply.github.com>
2024-03-17 12:30:23 +0600

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,13 +1,18 @@
-findViewById<ImageButton>(R.id.repeatBtn).setOnClickListener {
-    if (repeat) {
-        repeat = false
-        player.repeatMode = Player.REPEAT_MODE_OFF
-        findViewById<ImageButton>(R.id.repeatBtn).setImageResource(com.github.vkay94.dtpv.R.drawable.exo_controls_repeat_off)
-        findViewById<ImageButton>(R.id.repeatBtn).setColorFilter(ContextCompat.getColor(this, R.color.your_color_off), PorterDuff.Mode.SRC_IN)
-    } else {
-        repeat = true
-        player.repeatMode = Player.REPEAT_MODE_ONE
-        findViewById<ImageButton>(R.id.repeatBtn).setImageResource(com.github.vkay94.dtpv.R.drawable.exo_controls_repeat_all)
-        findViewById<ImageButton>(R.id.repeatBtn).setColorFilter(ContextCompat.getColor(this, R.color.your_color_on), PorterDuff.Mode.SRC_IN)
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://www.jitpack.io" ) }
+    }
+}
+
+rootProject.name = "My Player"
+include(":app")
