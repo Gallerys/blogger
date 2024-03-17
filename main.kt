625bccfe60e1f68a40aa96384952e4a5a1a69080
Gallery <59381987+Gallerys@users.noreply.github.com>
2024-03-17 11:40:41 +0600

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,1 +1,12 @@
-
+findViewById<ImageButton>(R.id.repeatBtn).setOnClickListener {
+            if(repeat){
+                repeat = false
+                player.repeatMode = Player.REPEAT_MODE_OFF
+                findViewById<ImageButton>(R.id.repeatBtn).setImageResource(com.github.vkay94.dtpv.R.drawable.exo_controls_repeat_off)
+            }
+            else{
+                repeat = true
+                player.repeatMode = Player.REPEAT_MODE_ONE
+                findViewById<ImageButton>(R.id.repeatBtn).setImageResource(com.github.vkay94.dtpv.R.drawable.exo_controls_repeat_all)
+            }
+        }
