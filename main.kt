8eeb2d87ced4c118dcae30c3d26726341ce60d3e
Gallery <59381987+Gallerys@users.noreply.github.com>
2024-03-17 12:30:39 +0600

    chat

--- main.kt
+++ main.kt
@@ -5,12 +5,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://www.jitpack.io" ) }
+        maven { url "https://www.jitpack.io" } // Removed uri() and fixed the URL
     }
 }
 
