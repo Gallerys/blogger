4c48afbb5666700c1caf34717c05d0996f782092
Gallery <59381987+Gallerys@users.noreply.github.com>
2024-03-18 14:00:24 +0600

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,19 +1,1 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url "https://www.jitpack.io" } // Removed uri() and fixed the URL
-    }
-}
-
-rootProject.name = "My Player"
-include(":app")
