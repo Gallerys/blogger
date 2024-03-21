6dfb570dd61690b60763e9ac4208130540ad47a0
Gallery <59381987+Gallerys@users.noreply.github.com>
2024-03-21 22:12:43 +0600

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,17 @@
+<blogPost>
+    <title>Title of the Blog Post</title>
+    <author>Author Name</author>
+    <date>Publication Date</date>
+    <content>
+        <![CDATA[
+            <p>This is the content of the blog post. You can include <strong>HTML</strong> tags here for formatting.</p>
+            <p>For example, you can add paragraphs, headings, lists, images, etc.</p>
+        ]]>
+    </content>
+    <tags>
+        <tag>Tag1</tag>
+        <tag>Tag2</tag>
+        <!-- Add more tags as needed -->
+    </tags>
+</blogPost>
 
